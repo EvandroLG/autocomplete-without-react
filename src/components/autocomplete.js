cebbd14799f1d250f01a74873ebdf1f0d37e7f13
Evandro Leopoldino Gonçalves <evandrolgoncalves@gmail.com>
2018-02-21 00:52:48 -0300

    add logic to show items on keyup

--- src/components/autocomplete.js
+++ src/components/autocomplete.js
@@ -11,7 +11,7 @@ class Autocomplete {
     this.elements.root = root;
   }
 
-  _createInputElement() {
+  _getInputElement() {
     let input = doc.createElement('input');
     input.setAttribute('type', 'text');
     input.setAttribute('id', 'autocomplete');
@@ -19,26 +19,45 @@ class Autocomplete {
     return input;
   }
 
-  _createListElement() {
+  _getListElement() {
     let list = doc.createElement('ul');
     list.className = 'autocomplete-list';
 
     return list;
   }
 
+  _createItem(value) {
+    let li = doc.createElement('li');
+    li.innerText = value;
+
+    this.elements.list.appendChild(li);
+  }
+
+  _cleanList() {
+    this.elements.list.innerHTML = '';
+  }
+
   _onInputKeyUp(e) {
-    console.log(this.search.getItems(e.target.value));
+    this._cleanList();
+
+    const value = e.target.value;
+    const that = this;
+
+    this.search.getItems(value).forEach((item) => {
+      that._createItem(item);
+    });
   }
 
   _events() {
-    this.elements.input.addEventListener('keyup', debounce(this._onInputKeyUp.bind(this), 300));
+    this.elements.input.addEventListener('keyup',
+      debounce(this._onInputKeyUp.bind(this), 300));
   }
 
   render() {
-    this.elements.input = this._createInputElement(); 
+    this.elements.input = this._getInputElement();
     this.elements.root.appendChild(this.elements.input);
 
-    this.elements.list = this._createListElement();
+    this.elements.list = this._getListElement();
     this.elements.root.appendChild(this.elements.list);
 
     this._events();
